b47e7814082ae33cf84d9b1a3fd0ba3094d89d67
loew-tech <loew.technology@gmail.com>
2026-08-15 15:31:08 -0600

    client getCategoriesActivities function

--- src/api/client.ts
+++ src/api/client.ts
@@ -7,21 +7,49 @@ import {
   API_Error,
   ClientArgumentError,
 } from "../common/errors";
+import type { Category } from "../models/Category";
 
 const API_URL = import.meta.env.VITE_API_URL;
 const INVALID_CATEGORY_CHARS = /[\\/?#%]/;
 
 export async function getCategories() {
-  const { tokens } = await fetchAuthSession();
-  if (!tokens?.idToken) {
-    throw new AuthenticationError();
-  }
+  const authHeader = await getAuthHeader();
 
   const response = await fetch(`${API_URL}/categories`, {
     headers: {
-      Authorization: `Bearer ${tokens.idToken.toString()}`,
+      Authorization: authHeader,
     },
   });
+
+  if (!response.ok) {
+    throw new API_Error(response.status);
+  }
+
+  return response.json();
+}
+
+export async function getCategoriesActivities(
+  categories: string[],
+): Promise<Category[]> {
+  const authHeader = await getAuthHeader();
+
+  return Promise.all(
+    categories.map((category) => getCategoryActivities(category, authHeader)),
+  );
+}
+
+export async function getCategoryActivities(
+  category: string,
+  authHeader: string,
+): Promise<Category> {
+  const response = await fetch(
+    `${API_URL}/categories/${encodeURIComponent(category)}`,
+    {
+      headers: {
+        Authorization: authHeader,
+      },
+    },
+  );
 
   if (!response.ok) {
     throw new API_Error(response.status);
@@ -42,23 +70,18 @@ export async function removeActivity(activity: Activity): Promise<boolean> {
   return false;
 }
 
-// @TODO: implement
 export async function addActivity(activity: Activity): Promise<boolean> {
-  const { tokens } = await fetchAuthSession();
-  if (!tokens?.idToken) {
-    throw new AuthenticationError();
-  }
-
   if (!isValidCategory(activity.category)) {
     throw new ClientArgumentError(CATEGORY);
   }
 
+  const authHeader = await getAuthHeader();
   const response = await fetch(
     `${API_URL}/activities/${encodeURIComponent(activity.category)}`,
     {
       method: "POST",
       headers: {
-        Authorization: `Bearer ${tokens.idToken.toString()}`,
+        Authorization: authHeader,
         "Content-Type": "application/json",
       },
       body: activityToJSONRequestObject(activity),
@@ -70,6 +93,14 @@ export async function addActivity(activity: Activity): Promise<boolean> {
   }
 
   return true;
+}
+
+async function getAuthHeader(): Promise<string> {
+  const { tokens } = await fetchAuthSession();
+  if (!tokens || !tokens?.idToken) {
+    throw new AuthenticationError();
+  }
+  return `Bearer ${tokens.idToken!.toString()}`;
 }
 
 function isValidCategory(category: string): boolean {
