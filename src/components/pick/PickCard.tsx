6ccedd8e8e24cf089cff6fccdc5acc6363872191
loew-tech <loew.technology@gmail.com>
2026-08-19 10:40:37 -0600

    improved visuals for PickCard

--- src/components/pick/PickCard.tsx
+++ src/components/pick/PickCard.tsx
@@ -1,5 +1,8 @@
-import { Paper, Typography } from "@mui/material";
+import { EmojiEvents } from "@mui/icons-material";
+import { Box, Paper, Typography } from "@mui/material";
+
 import type { Pick } from "../../models/Pick";
+import "./PickCard.scss";
 
 interface PickCardProps {
   pick: Pick;
@@ -7,12 +10,36 @@ interface PickCardProps {
 
 const PickCard = ({ pick }: PickCardProps) => {
   return (
-    <Paper elevation={4} sx={{ p: 2, backgroundColor: "gold" }}>
-      <Typography variant="h6">{pick.category}</Typography>
-      <Typography variant="h6" sx={{ color: "blue" }}>
+    <Paper className="pick-card" elevation={4}>
+      <Box className="pick-card__icon">
+        <EmojiEvents />
+      </Box>
+
+      <Typography
+        className="pick-card__label"
+        variant="overline"
+        color="text.secondary"
+      >
+        Your Pick
+      </Typography>
+
+      <Typography
+        className="pick-card__name"
+        variant="h4"
+        component="h2"
+      >
         {pick.name}
+      </Typography>
+
+      <Typography
+        className="pick-card__category"
+        variant="subtitle1"
+        color="text.secondary"
+      >
+        {pick.category}
       </Typography>
     </Paper>
   );
 };
+
 export default PickCard;
