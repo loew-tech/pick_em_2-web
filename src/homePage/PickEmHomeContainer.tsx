2d1c417c8b443be1c2f6c043946b99da2e85a894
loew-tech <loew.technology@gmail.com>
2026-08-19 10:24:51 -0600

    improvements to visuals of home page

--- src/homePage/PickEmHomeContainer.tsx
+++ src/homePage/PickEmHomeContainer.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Container, Paper } from "@mui/material";
+import { Box, Container, Stack, Typography } from "@mui/material";
 
 import CategorySelectForm from "../components/categories/CategoriesSelectForm";
 import { FilterDropdown } from "../components/activity/FilterDropdown";
@@ -12,9 +12,12 @@ import PickCard from "../components/pick/PickCard";
 import { getPick } from "../api/client";
 import { EFFORT, INTEREST } from "../common/constants";
 
+import "./PickEmHomeContainer.scss";
+
 interface PickEmHomeContainerProps {
   setError: (s: string) => void;
 }
+
 const PickEmHomeContainer = ({ setError }: PickEmHomeContainerProps) => {
   const [selectedCategoriesIds, setSelectedCategoriesIds] = useState<string[]>(
     [],
@@ -34,6 +37,7 @@ const PickEmHomeContainer = ({ setError }: PickEmHomeContainerProps) => {
 
   const makePick = async () => {
     setError("");
+
     try {
       const pick = await getPick(selectedCategoriesIds, interest, effort);
       setPick(pick);
@@ -45,42 +49,86 @@ const PickEmHomeContainer = ({ setError }: PickEmHomeContainerProps) => {
 
   const addToSelection = (event: React.ChangeEvent<HTMLInputElement>) => {
     let new_ = [...selectedCategoriesIds];
+
     if (event.target.checked) {
       new_.push(event.target.name);
     } else {
       new_ = new_.filter((v) => v !== event.target.name);
     }
+
     setSelectedCategoriesIds(new_);
   };
 
   return (
-    <Container maxWidth="md">
-      <Paper elevation={3} sx={{ mt: 8, p: 4 }}>
-        <CategorySelectForm onSelect={addToSelection} setError={setError} />
-        <FilterDropdown
-          title={INTEREST}
-          handleChange={handleInterestChange}
-          value={interest}
-        />
-        <FilterDropdown
-          title={EFFORT}
-          handleChange={handleEffortChange}
-          value={effort}
-        />
-        <PickEmHomeActionButtons
-          selectedCategoriesIds={selectedCategoriesIds}
-          setSelectedCategories={setSelectedCategories}
-          interest={interest}
-          effort={effort}
-          makePick={makePick}
-          setError={setError}
-        />
-      </Paper>
-      {pick && <PickCard pick={pick} />}
-      {selectedCategories.map((category) => (
-        <CategoriesActivitesContainer key={category.id} category={category} />
-      ))}
-    </Container>
+    <Box className="pick-em-home">
+      <Container maxWidth="md">
+        <Box className="pick-em-home__preferences">
+          <Box className="pick-em-home__categories">
+            <Typography variant="h6" component="h2">
+              What are you interested in?
+            </Typography>
+
+            <CategorySelectForm onSelect={addToSelection} setError={setError} />
+          </Box>
+
+          <Box className="pick-em-home__controls">
+            <Box className="pick-em-home__filters">
+              <Typography variant="h6" component="h2">
+                How are you feeling?
+              </Typography>
+
+              <Stack spacing={2}>
+                <FilterDropdown
+                  title={INTEREST}
+                  handleChange={handleInterestChange}
+                  value={interest}
+                />
+
+                <FilterDropdown
+                  title={EFFORT}
+                  handleChange={handleEffortChange}
+                  value={effort}
+                />
+              </Stack>
+            </Box>
+
+            <Box className="pick-em-home__actions">
+              <PickEmHomeActionButtons
+                selectedCategoriesIds={selectedCategoriesIds}
+                setSelectedCategories={setSelectedCategories}
+                interest={interest}
+                effort={effort}
+                makePick={makePick}
+                setError={setError}
+              />
+            </Box>
+            {pick && (
+              <Box className="pick-em-home__result">
+                <PickCard pick={pick} />
+              </Box>
+            )}
+          </Box>
+        </Box>
+
+        {selectedCategories.length > 0 && (
+          <Box className="pick-em-home__activities">
+            <Typography variant="h5" component="h2">
+              Your activities
+            </Typography>
+
+            <Stack spacing={2}>
+              {selectedCategories.map((category) => (
+                <CategoriesActivitesContainer
+                  key={category.id}
+                  category={category}
+                />
+              ))}
+            </Stack>
+          </Box>
+        )}
+      </Container>
+    </Box>
   );
 };
+
 export default PickEmHomeContainer;
